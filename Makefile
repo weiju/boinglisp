b76f57cefdcfba2a9cee64e9e5ba34deb6bde787
Wei-ju Wu <weiju.wu@gmail.com>
2015-10-14 18:49:06 -0700

    using includes and macros in assembler

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
-ASM_FLAGS = -Fhunk -devpac -I/home/weiju/Development/NDK_3.9/Include/include_i
+ASM_FLAGS = -Fhunk -devpac -I/home/weiju/Development/amigadev/NDK_3.9/Include/include_i
+VLINK_FLAGS = -L/home/weiju/local/vbcc/targets/m68k-amigaos/lib -bamigahunk -lamiga
 ASM = vasmm68k_mot
 .SUFFIXES : .o .asm
 
@@ -11,4 +12,4 @@ clean:
 
 
 runtime: runtime.o
-	vlink -bamigahunk -o $@ -s $<
+	vlink $(VLINK_FLAGS) -o $@ -s $<
--- runtime.asm
+++ runtime.asm
@@ -1,32 +1,29 @@
 ;; Exec
 ABSEXECBASE		EQU	4
-_LVOOpenLibrary		EQU	-552
-_LVOCloseLibrary	EQU	-414
-;; DOS
-_LVOOutput		EQU	-60
-_LVOWrite		EQU	-48
+
+        INCLUDE "exec/exec.i"
 
 start:
-	move.l	4.w,a6
-	lea	dosname(pc),a1
-	moveq	#0,d0
-	jsr	_LVOOpenLibrary(a6)
-	tst.l	d0
-	beq.s	nodos
-	move.l	d0,a6
-	lea	msg(pc),a0
-	moveq	#-1,d3
-	move.l	a0,d2
+        move.l	ABSEXECBASE.w,a6
+	    lea	dosname(pc),a1
+	    moveq	#0,d0
+        JSRLIB  OpenLibrary
+	    tst.l	d0
+	    beq.s	nodos
+	    move.l	d0,a6
+	    lea	msg(pc),a0
+	    moveq	#-1,d3
+	    move.l	a0,d2
 strlen:
-	addq.l	#1,d3
-	tst.b	(a0)+
-	bne.s	strlen
-	jsr	_LVOOutput(a6)
-	move.l	d0,d1
-	jsr	_LVOWrite(a6)
-	move.l	a6,a1
-	move.l	4.w,a6
-	jsr	_LVOCloseLibrary(a6)
+	    addq.l	#1,d3
+	    tst.b	(a0)+
+	    bne.s	strlen
+        JSRLIB  Output
+	    move.l	d0,d1
+        JSRLIB  Write
+	    move.l	a6,a1
+	    move.l	ABSEXECBASE.w,a6
+        JSRLIB  CloseLibrary
 nodos:
 	moveq	#0,d0
   	jsr	dummy
